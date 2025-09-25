d6a353535c4f05a8e4903c378c8c841da1ae4022
reagancheatham <reagan.cheatham@eagles.oc.edu>
2025-09-25 15:41:04 -0500

    Working on recreating backend

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request.');
+});
+
+app.put('/user', (req, res) => {
+    res.send('Got a PUT request at /user.');
+});
+
+app.delete('/user', (req, res) => {
+    res.send('Got a DELETE request at /user.');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
